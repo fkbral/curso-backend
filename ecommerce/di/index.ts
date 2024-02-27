062c27776bbedef7b86fd6718f1ba826c49dd75a
Felipe Cabral <felipeoc5@hotmail.com>
2024-02-27 15:46:26 -0300

    feat: aula 40 - setup testes

--- ecommerce/di/index.ts
+++ ecommerce/di/index.ts
@@ -1,9 +1,17 @@
-import { container } from 'tsyringe'
-import { CostumersRepository, ICostumersRepository } from '../repositories/costumers/CostumersRepository'
-import { IOrderProductsRepository } from './fakes'
-import { IProductsRepository, ProductsRepository } from '../repositories/orders/ProductsRepository'
-import { OrderProductsRepository } from '../repositories/orders/OrderProductsRepository'
+import { container } from "tsyringe";
+import {
+  CostumersRepository,
+  ICostumersRepository,
+} from "../repositories/costumers/CostumersRepository";
+import {
+  IProductsRepository,
+  ProductsRepository,
+} from "../repositories/orders/ProductsRepository";
+import {
+  IOrderProductsRepository,
+  OrderProductsRepository,
+} from "../repositories/orders/OrderProductsRepository";
 
-container.register(ICostumersRepository, CostumersRepository)
-container.register(IProductsRepository, ProductsRepository)
-container.register(IOrderProductsRepository, OrderProductsRepository)
+container.register(ICostumersRepository, CostumersRepository);
+container.register(IProductsRepository, ProductsRepository);
+container.register(IOrderProductsRepository, OrderProductsRepository);
--- ecommerce/repositories/orders/OrderProductsRepository.ts
+++ ecommerce/repositories/orders/OrderProductsRepository.ts
@@ -1,14 +1,14 @@
 import { prismaClient } from "../../prismaClient";
 import { CreateOrderProduct, OrderProduct } from "../../types/OrderProducts";
 
-export const IOrderProductsRepository = "IOrderProductsRepository"
+export const IOrderProductsRepository = "IOrderProductsRepository";
 export interface IOrderProductsRepository {
-    create: (data: CreateOrderProduct) => Promise<OrderProduct>
+  create: (data: CreateOrderProduct) => Promise<OrderProduct>;
 }
 
 export class OrderProductsRepository implements IOrderProductsRepository {
-    async create (data: CreateOrderProduct) : Promise<OrderProduct> {
-        const orderProduct = await prismaClient.order_products.create({ data });
-        return orderProduct
-    } ;
+  async create(data: CreateOrderProduct): Promise<OrderProduct> {
+    const orderProduct = await prismaClient.order_products.create({ data });
+    return orderProduct;
+  }
 }
