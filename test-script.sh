9347c292a5151f2eade50ea12b0f77eb1fb90412
Felipe Cabral <felipeoc5@hotmail.com>
2024-02-27 16:33:45 -0300

    test

--- test-script.sh
+++ test-script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source ./.env.test
 docker-compose --file ./docker-compose-test.yml up -d postgresdb
 DATABASE_URL=postgresql://postgres:password@localhost:5438/testsdb?schema=public npx prisma db push
 npm run test
